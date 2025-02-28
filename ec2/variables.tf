e912171c7f9e82b4d44e080d991d0270c79d02af
kabiratnas <153971340+kabiratnas@users.noreply.github.com>
2025-02-27 20:57:44 -0600

    merged from dennis branch

--- ec2/variables.tf
+++ ec2/variables.tf
@@ -9,7 +9,7 @@ variable "key_name" {
 }
 
 variable "instance_type" { 
-    description = "instance type for ec2" 
+    description = "instance type for ec2"
     default     =  "t2.medium" 
 }
 
@@ -19,10 +19,19 @@ variable "security_group" {
 }
 
 variable "tag_name" { 
-    description = "Tag Name of for Ec2 instance" 
+    description = "Tag Name of for Ec2 instance"
     default     = "terraform-ec2" 
 } 
 variable "ami_id" { 
+<<<<<<< HEAD
     description = "AMI for Ubuntu Ec2 instance" 
     default     = "ami-0e1bed4f06a3b463d" 
+=======
+    description = "AMI for Ec2 instance" 
+    default     = "ami-0893df1bd754c3189" 
+>>>>>>> dennis-branch
+}
+variable "instance_count" {
+  description = "Number of EC2 instances to create"
+  type        = number
 }
