23fb50c1eb589520dd40e047baec1b34769f7c27
tiwaib <tiwai@techsynnovate.com>
2025-03-28 17:27:28 -0500

    Update variables.tf

--- ec2/variables.tf
+++ ec2/variables.tf
@@ -15,7 +15,7 @@ variable "instance_type" {
 
 variable "security_group" { 
     description = "Name of security group" 
-    default     = "new-terraform-sg" 
+    default     = "tf-sg" 
 }
 
 variable "ami_id" { 
